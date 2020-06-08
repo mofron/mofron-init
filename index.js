540c6c42092d113d341ba2960daf6dde22b50f79
simpart <simpart>
2020-06-08 23:36:20 +0000

    mod index.html copy target

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ if (undefined !== process.argv[2]) {
     try { fs.mkdirSync(tgt); } catch (e) {}
 }
 
-fs.copyFile(__dirname + "/tmpl/index.html", tgt + "/index.html",
-    (err) => {
-        if (err) {
-	    console.error(err);
-	}
-    }
-);
-
 try { fs.mkdirSync(tgt + "/conf"); } catch (e) {}
 fs.copyFile(__dirname + "/tmpl/webpack.config.index.js", tgt + "/conf/webpack.config.index.js",
     (err) => {
@@ -37,6 +29,13 @@ fs.copyFile(__dirname + "/tmpl/index.mf", tgt + "/mof/index.mf",
 
 try { fs.mkdirSync(tgt + "/dist"); } catch (e) {}
 try { fs.mkdirSync(tgt + "/dist/index"); } catch (e) {}
+fs.copyFile(__dirname + "/tmpl/index.html", tgt + "/dist/index/index.html",
+    (err) => {
+        if (err) {
+            console.error(err);
+        }
+    }
+);
 
 console.log("mofron-init was finished.");
 console.log("please execute those bellow commands to complete the initialization.\n");
